687b9f3e7020e72007c563b9d3ba4cbfff8c7539
RomanKrajewski <s0553506@htw-berlin.de>
2020-06-15 14:57:28 +0200

    implement zoom and highlighting

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -7,9 +7,6 @@
 
 <script>
     import * as d3 from "d3"
-    import {bboxCollide} from "d3-bboxCollide"
-    import data from '../assets/data.json'
-
     export default {
         name: 'Chart',
         props: {
@@ -21,21 +18,15 @@
                 width: 1600,
                 maxLinkCount: Math.max(...data.nodes.map(d => this.getLinkCount(d.id))),
                 maxWeight: Math.max(...data.links.map(d => d.weight)),
-                maxScaleFactor: 4000
-            }
-        },
-        computed: {
-            weightMedian: function(){
-                const weights = data.links.map((d)=> d.weight).sort()
-                return weights[Math.floor(weights.length/2)]
-            },
-            weightAverage: function(){
-                const arr = (data.links.map((d)=> d.weight))
-                return arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
+                maxScaleFactor: 4000,
+                nodes: Array,
+                renderedNodes: Object,
+                simulation: Object,
+                pimmelpommel: Object
             }
         },
         methods: {
-            drag (simulation){
+            drag(simulation) {
                 function dragstarted(d) {
                     if (!d3.event.active) simulation.alphaTarget(0.2).restart();
                     d.fx = d.x;
@@ -63,6 +54,42 @@
                     return link.source === nodeId || link.target === nodeId
                 }).length
             },
+            highlightNodes(searchString){
+                this.nodes.forEach((node) => {
+                    if(node.name.includes(searchString)){
+                        node.highlighted = true
+                    }else{
+                        node.highlighted = false
+                    }
+                })
+                this.pimmelpommel = this.renderedNodes.selectAll("g")
+                    .data(this.nodes)
+                    .join(enter=>
+                            enter.append("g")
+                            .each(function (d) {
+                                const text = d3.select(this).append("svg:text")
+                                    .text(d.name)
+                                    .attr("fill", "black")
+                                    .attr("font-size", `${d.bbox.scaleFactor}em`)
+                                    .attr("text-anchor", "middle")
+                                    .attr("text-rendering", "optimizeSpeed")
+                                const bbox = text.node().getBBox()
+                                d3.select(this).append("rect")
+                                    .attr("fill", "wheat")
+                                    .attr("fill-opacity", 0.8)
+                                    .attr("width", bbox.width)
+                                    .attr("height", bbox.height)
+                                    .attr("x", bbox.x)
+                                    .attr("y", bbox.y)
+                                    .attr("shape-rendering", "optimizeSpeed")
+                                text.raise()
+                            })
+                            .call(this.drag(this.simulation)),
+                        update => update
+                            .each(function (d) {
+                                d3.select(this).select("rect").attr("fill", d.highlighted?"red":"wheat")
+                            }))
+            },
             getTextBBox(textElement, node) {
                 const areaScaleFactor = this.maxScaleFactor * (this.getLinkCount(node.id) / this.maxLinkCount)
                 const baseBBox = textElement.attr("font-size", "1em").text(node.name).node().getBBox()
@@ -81,62 +108,53 @@
 
                 const sampleBBox = svg.append("text")
                 const links = data.links.map(d => Object.create(d));
-                const nodes = data.nodes.map(d => Object.assign({bbox: this.getTextBBox(sampleBBox, d)}, d));
+                this.nodes = data.nodes.map(d => Object.assign({highlighted:true, bbox: this.getTextBBox(sampleBBox, d)}, d));
                 sampleBBox.remove()
 
                 const collide = bboxCollide((d) => [[-d.bbox.width / 2, -d.bbox.height / 2], [d.bbox.width / 2, d.bbox.height / 2]])
                     .strength(0.1)
-                    .iterations(2)
+                    .iterations(1)
 
-                const simulation = d3.forceSimulation(nodes)
-                    .force("link", d3.forceLink(links).distance((d) =>   30 - (Math.log(d.weight)/Math.log(this.maxWeight))*40))
+                this.simulation = d3.forceSimulation(this.nodes)
+                    .force("link", d3.forceLink(links).distance((d) => 30 - (Math.log(d.weight) / Math.log(this.maxWeight)) * 40))
                     .force("charge", d3.forceManyBody().strength(-60).distanceMax(100))
                     .force("collision", collide)
                     .force("center", d3.forceCenter(this.width / 2, this.height / 2))
-                    .force("box", () => nodes.forEach((d) => {
+                    .force("box", () => this.nodes.forEach((d) => {
                             d.x = Math.max(d.bbox.width / 2, Math.min(this.width - d.bbox.width / 2, d.x))
                             d.y = Math.max(d.bbox.height / 2, Math.min(this.height - d.bbox.height / 2, d.y));
                         })
                     )
 
+                const zoomGroup = svg.append("g")
+                // .attr("shape-rendering", "optimizeSpeed")
 
-                const link = svg.append("g")
+                svg.call(d3.zoom()
+                    .extent([[0, 0], [this.width, this.height]])
+                    .scaleExtent([1, 3])
+                    .on("zoom", () => zoomGroup.attr("transform", d3.event.transform)));
+
+                const link = zoomGroup.append("g")
                     .attr("stroke", "#999")
                     .attr("stroke-opacity", 0.6)
                     .selectAll("line")
                     .data(links)
                     .join("line")
-                    .attr("stroke-width", d => d.weight);
+                    .attr("stroke-width", d => d.weight)
+                    .attr("shape-rendering", "optimizeSpeed")
 
-                const node = svg.append("g")
-                    .selectAll("g")
-                    .data(nodes)
-                    .join("g")
-                    .each(function (d) {
-                        const text = d3.select(this).append("svg:text")
-                            .text(d.name)
-                            .attr("fill", "black")
-                            .attr("font-size", `${d.bbox.scaleFactor}em`)
-                            .attr("text-anchor", "middle")
-                        const bbox = text.node().getBBox()
-                        d3.select(this).append("rect")
-                            .attr("fill", "wheat")
-                            .attr("width", bbox.width)
-                            .attr("height", bbox.height)
-                            .attr("x", bbox.x)
-                            .attr("y", bbox.y)
-                        text.raise()
-                    })
-                    .call(this.drag(simulation));
+                this.renderedNodes = zoomGroup.append("g")
 
-                simulation.on("tick", () => {
+                this.highlightNodes("Aar")
+
+                this.simulation.on("tick", () => {
                     link
                         .attr("x1", d => d.source.x)
                         .attr("y1", d => d.source.y)
                         .attr("x2", d => d.target.x)
                         .attr("y2", d => d.target.y);
 
-                    node
+                    this.pimmelpommel
                         .attr("transform", (d) => `translate(${d.x},${d.y})`)
 
                 });
@@ -144,16 +162,21 @@
             }
 
         },
+        // console.log(this.weightMedian)
         mounted() {
-            // console.log(this.weightMedian)
             // console.log(this.weightAverage)
             this.createChart();
+            this.highlightNodes("Aar")
         }
     }
+    import {bboxCollide} from "d3-bboxCollide"
+
+    import data from '../assets/data.json'
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Lato|Open+Sans|Oswald|Raleway|Roboto|Indie+Flower|Gamja+Flower');
     h3 {
         margin: 40px 0 0;
     }
@@ -170,5 +193,13 @@
 
     a {
         color: #42b983;
+    }
+
+    text {
+        will-change: transform;
+    }
+
+    g {
+        will-change: transform;
     }
 </style>
